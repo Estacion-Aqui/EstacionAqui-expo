67b615e68a5bf0e21c5e2395d2c75e99c6f81d34
icrxz <igor.cruz@mercadolivre.com>
2021-09-24 22:20:33 -0300

    update setup screen

--- src/screens/Setup/index.tsx
+++ src/screens/Setup/index.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-
 import { Button } from 'react-native-elements';
 import { Modular } from '../../components/Modular';
 
 import { RFValue } from 'react-native-responsive-fontsize';
 import theme from '../../global/styles/theme';
 
-import Input from 'react-native-input-style';
+import React, { useCallback, useState, useEffect } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import {TravelData, ParkData, UserData} from '../../global/scripts/apis';
+import {getDBEstabData, checkUserData, logouts, setUserData, checkDataLogin} from '../../global/scripts/database';
+
+import { useNavigation } from '@react-navigation/native';
 
 import {
-    TouchableOpacity,
-    TouchableOpacityProps,
-    Text,
-    View,
+    ActivityIndicator, 
+    Alert,
+    TextInput,
     StyleSheet
 } from 'react-native';
 
@@ -35,9 +37,98 @@ import {
 import { Header } from '../../components/Header';
 
 export function Setup({ navigation }){
-  function onChangeData(){
 
+  const [usData, setData] = useState<UserData>();
+  const [usDataName, setName] = useState<string>();
+  const [usDataCar, setCar] = useState<string>();
+  const [usDataEmail, setEmail] = useState<string>();
+  const [usDataPassword, setPassword] = useState<string>();
+  function getData(){
+    checkUserData().then(function(result){
+      if(result != null)
+        setData(result);
+      else
+        setData(undefined);
+    });
   }
+
+ function logout(){
+    logouts();
+    setData(undefined);
+ }
+ function loginSucess(item : UserData){
+  Alert.alert(
+    "Login Realizado com Sucesso",
+    "",
+    [
+      { text: "OK", onPress: () => console.log("OK Pressed") }
+    ]
+  );
+  setData({id : item.id, name : item.name,email : item.email,car : item.car,password : item.password});
+
+ }
+ function checkData(){
+   if(usDataName && usDataCar && usDataEmail && usDataPassword){
+    setUserData('', usDataName, usDataCar, usDataEmail, usDataPassword).then(function(item){
+      if(!item.id){
+        Alert.alert(
+          "Usuario Invalido",
+          "Usuario invalido favor realizar novamente a solicitação",
+          [
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ]
+        );
+      }else{
+        loginSucess(item);
+      }
+    })
+
+   }else{
+      Alert.alert(
+      "Preencha todos os dados",
+      "Favor preencher os dados de Nome, Email, Carro e Senha!!",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
+   }
+
+ }
+ function checkDataLogins(){
+   if(usDataEmail && usDataPassword){
+    checkDataLogin(usDataEmail, usDataPassword).then(function(item){
+      if(!item.id){
+        Alert.alert(
+          "Usuario Invalido",
+          "Usuario invalido favor realizar novamente a solicitação",
+          [
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ]
+        );
+      }else{
+        setName(item.name);
+        setCar(item.car);
+        loginSucess(item);
+      }
+    })
+   }else{
+      Alert.alert(
+      "Preencha todos os dados",
+      "Favor preencher os dados de Nome, Email, Carro e Senha!!",
+      [
+        { text: "OK", onPress: () => console.log("OK Pressed") }
+      ]
+    );
+   }
+ }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useFocusEffect(useCallback(() => {
+    getData()
+  },[]));
   return (
     <Container> 
       <HeaderStyle>
@@ -47,68 +138,72 @@ export function Setup({ navigation }){
               <LogoutButton onPress={() => {}}>
                 <Icon name="edit"/>
               </LogoutButton>
-              <Photo source={{ uri: 'https://avatars.githubusercontent.com/GTeixeirinha7?v=4'}}/>
+              {/* https://avatars.githubusercontent.com/GTeixeirinha7?v=4 */}
+              <Photo source={{ uri: 'https://www.pinpng.com/pngs/m/341-3415688_no-avatar-png-transparent-png.png'}}/>
             </ContainerPhoto>
           </UserInfo>
         </UserWrapper>
       </HeaderStyle>
       <ContentView>
         <ContentViewInput>        
-          <Input
-            id="name"
-            label="Nome Completo"
-            required
-            contain=" "
-            initialValue="Gabriel Teixeira"
-            outlined
-            borderColor={theme.colors.primary}
-            inputStyle={styles.inputs}
-            labelStyle={styles.inputsLabel}
-            formControlStyle={styles.inputsLabel} 
-            onInputChange={onChangeData}
+          <TextInput
+            placeholder="Nome Completo"
+            onChangeText={text => setName(text)}
+            defaultValue={usData == null ? "" : usData.name}
+            style={styles.inputs}
+            keyboardType="default"
           />
-
-          <Input
-            id="car"
-            label="Carro"
-            required
-            contain=" "
-            initialValue="Audi A3"
-            outlined
-            borderColor={theme.colors.primary}
-            inputStyle={styles.inputs}
-            labelStyle={styles.inputsLabel}
-            formControlStyle={styles.inputsLabel} 
-            onInputChange={onChangeData}
+          <TextInput
+            placeholder="Carro"
+            onChangeText={text => setCar(text)}
+            defaultValue={usData == null ? "" : usData.car}
+            style={styles.inputs}
+            keyboardType="default"
           />
-
-          <Input
-            id="email"
-            label="Email"
-            required
-            contain=" "
-            initialValue="gabrielteixeir137@gmail.com"
-            outlined
-            initia
-            borderColor={theme.colors.primary}
-            inputStyle={styles.inputs}
-            labelStyle={styles.inputsLabel}
-            formControlStyle={styles.inputsLabel} 
-            onInputChange={onChangeData}
+          <TextInput
+            placeholder="Email"
+            onChangeText={text => setEmail(text)}
+            defaultValue={usData == null ? "" : usData.email}
+            style={styles.inputs}
+            keyboardType="default"
           />
         </ContentViewInput>
-        
-        <Button
-          buttonStyle={styles.buttonStyle}
-          titleStyle={styles.buttonText}
-          title="Esqueci minha senha"
-        />
+        {
+          usData == null ? 
+        <>
+          <ContentViewInput>     
+          <TextInput
+            placeholder="Senha"
+            onChangeText={text => setPassword(text)}
+            style={styles.inputs}
+            secureTextEntry={true}
+            keyboardType="default"
+          />
+          </ContentViewInput>
+          <Button
+            buttonStyle={styles.buttonStyle}
+            titleStyle={styles.buttonText}
+            onPress = {() => checkDataLogins()}
+            title="Entrar"
+          />
 
-        <Button
-          buttonStyle={styles.buttonAttentionStyle}
-          titleStyle={styles.buttonText}
-          title="Sair"
-        />
+          <Button
+            buttonStyle={styles.buttonStyle}
+            titleStyle={styles.buttonText}
+            onPress = {() => checkData()}
+            title="Se Cadastrar"
+          />
+        </>
+          :
+        <>
+          <Button
+            buttonStyle={styles.buttonAttentionStyle}
+            titleStyle={styles.buttonText}
+            title="Sair"
+            onPress = {() => logout()}
+          />
+        </>
+        }
 
       </ContentView> 
       <Modular/>
@@ -121,6 +216,12 @@ const styles = StyleSheet.create({
       width: '100%',
       fontSize: RFValue(12),
       textAlignVertical: 'center',
+      backgroundColor: theme.colors.background,
+      color: theme.colors.empty,
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
     },
     buttonAttentionStyle: {
       backgroundColor: theme.colors.attention,
