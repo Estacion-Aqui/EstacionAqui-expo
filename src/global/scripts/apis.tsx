32ecf8f6b7ddc27133e0cfbbd21823dd72c4e9ea
Gteixeirinha7 <johnny.ayres@live.com>
2021-09-24 21:58:37 -0300

    finish integration with Database

--- src/global/scripts/apis.tsx
+++ src/global/scripts/apis.tsx
@@ -1,6 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
+import axios  from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import uuid from 'react-native-uuid';
+
+var token = '';
+const api_key = 'SmartParkingnmdtc78b87trfio8ufy';
+
+const api = axios.create({
+    baseURL : 'http://192.168.0.147:3333',
+    headers: {Authorization: `Bearer ${token}` }
+});
+
+const api_oauth = axios.create({
+    baseURL : 'http://192.168.0.147:3333'
+});
+
+export interface TravelData {
+  id: string;
+  spotId : string;
+  estabId : string;
+  day : string;
+  confirmed: Boolean;
+  cancelled: Boolean;
+}
 
 export interface ParkData {
   id: string;
@@ -8,49 +29,143 @@ export interface ParkData {
   title : string;
   amount : string;
   quantitySpots : string;
+  latitude : number;   
+  longitude : number;
+}
+export interface UserData {
+  id: string;
+  name : string;
+  car : string;
+  password : string;
+  email : string;
 }
 
-export function getAPIAllPlaces(){
-    const data = {
-        id: String(uuid.v4()),
-        type : 'open',
-        title : '2.5 Km de distancia',
-        amount : 'Termomecanica',
-        quantitySpots : '10 vagas disponíves'
-    } as ParkData
-    
-    const data1 = {
-        id: String(uuid.v4()),
-        type : 'closed',
-        title : '14.5 Km de distancia',
-        amount : 'Termomecanica - Ensino Fundamental',
-        quantitySpots : 'Estabelecimento Fechado'
-    } as ParkData
-    
-    const data2 = {
-        id: String(uuid.v4()),
-        type : 'empty',
-        title : '3.5 Km de distancia',
-        amount : 'Termomecanica - Apresentação da Banda 2021',
-        quantitySpots : 'Evento Encerrado'
-    } as ParkData
+export function oauth(){
+    try {
+        api_oauth.post('/oauth', {api_key: api_key})
+        .then(function(resp){
+            console.log('resp'+resp);
+            token = resp.data.token;
+        })
+        .catch((err) => {
+          console.error("ops! ocorreu um erro" + err);
+        }).finally(()=> {
+        });
+    } catch (error) {
+        console.log(error);
+    }   
+}
+export async function getAPIAllPlaces(pkData :ParkData[]){
+    var respMap = new Map();
+    try {
+        var resp  = await api.get('/places');
+            console.log('resp'+resp);
+            var respData = (resp.data);
+            
+            respData.forEach(function(item : ParkData){
+                respMap.set(item.id, item);
+                pkData.push(item);
+            }, {respMap})
 
-    const data3 = {
-        id: String(uuid.v4()),
-        type : 'empty',
-        title : '3.5 Km de distancia',
-        amount : 'Shopping Metropole',
-        quantitySpots : 'Evento Encerrado'
-    } as ParkData
-    
-    const data4 = {
-        id: String(uuid.v4()),
-        type : 'empty',
-        title : '3.5 Km de distancia',
-        amount : 'Shopping Metropole',
-        quantitySpots : 'Evento Encerrado'
-    } as ParkData
+            pkData.forEach(function(item){
+                if(respMap.has(item.id)){
+                    item.type          = respMap.get(item.id).type;
+                    item.title         = respMap.get(item.id).title;
+                    item.amount        = respMap.get(item.id).amount;
+                    item.quantitySpots = respMap.get(item.id).quantitySpots;
+                    item.latitude = respMap.get(item.id).latitude;
+                    item.longitude = respMap.get(item.id).longitude;
+                }else{
+                    item.type = 'closed';
+                }
+                respMap.set(item.id, item);
+            });
+            return respMap;
+    } catch (error) {
+        console.log(error);
+    }  
+    return respMap;
 
-    
-  return [data, data1, data2, data3, data4];
+}
+export async function lastTravels(usId : String){
+    var respData : TravelData[];
+    respData = [];
+    try {
+        var resp  = await api.get(`/travelData?usId=${usId}`);
+        respData = (resp.data);
+    } catch (error) {
+        console.log(error);
+    }   
+    return respData;
+}
+export async function checkLogin(emails : string, password: string){
+    try {
+        // var resp = await api.post('/checkLogin', ({email: emails, password: password}));
+        var resp = await api.get('/checkLogin');
+        console.log('resp'+resp.data);
+        return resp.data;
+    } catch (error) {
+        console.log(error);
+    }   
+    return null;
+}
+export async function sendUserData(us : UserData){
+    try {
+        // var resp = await api.post((us.id ? '/updateData' : '/insertData'), JSON.stringify(us));
+        var resp = await api.get((us.id ? '/updateData' : '/insertData'));
+        console.log('resp'+resp.data);
+        return resp.data;
+    } catch (error) {
+        console.log(error);
+    }   
+    return null;
+}
+export async function reserveSpot(parkId : string, usId : String){
+    var respData : TravelData;
+
+
+    var date = new Date().getDate();
+    var month = new Date().getMonth();
+    var year = new Date().getFullYear();
+
+    respData ={
+        id : '',
+        spotId : '',
+        estabId: parkId,
+        day : ( date + '/' + month + '/' + year),
+        cancelled: false,
+        confirmed: false
+    }
+    try {
+        // var resp = await api.get(`/reserveSpot?user=${usId}&parkId=${parkId}`);
+        var resp = await api.get(`/reserveSpot`);
+        respData.spotId = resp.data.spotId;
+        respData.id = resp.data.id;
+        return respData;
+    } catch (error) {
+        console.log(error);
+    }   
+    return respData;
+}
+export async function confirmSpot(parkId : String, spotId : String, usId : String){
+    var data = {parkId: parkId, spotId: spotId, usId: usId};
+    return await spotData(data, '/confirmSpot');
+}
+export async function cancelSpot(parkId : String, usId : String){
+    var data = {parkId: parkId, usId: usId};
+    return await spotData(data, '/cancelSpot');
+}
+export async function checkSpot(parkId : String, spotId : String, usId : String){
+    var data = {parkId: parkId, spotId: spotId, usId: usId};
+    return await spotData(data, '/checkStatusSpot');
+}
+export async function spotData(jsonzada : Object, endpoint : string){
+    try {
+        var resp  = await api.post(endpoint, jsonzada);
+        console.log('resp'+resp);
+        return resp.data;
+    } catch (error) {
+        console.log(error);
+    }   
+    return null;
 }
--- src/global/scripts/database.tsx
+++ src/global/scripts/database.tsx
@@ -1,35 +1,156 @@
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import uuid from 'react-native-uuid';
+import {useNetInfo} from "@react-native-community/netinfo";
 
-import {getAPIAllPlaces, ParkData} from '../scripts/apis';
+import {getAPIAllPlaces, ParkData, lastTravels, TravelData, UserData, sendUserData, checkLogin, confirmSpot, cancelSpot, checkSpot} from '../scripts/apis';
 
-export function getDBAllPlaces(){
-    const dataKey = '@EstacionAqui:places';
+const placesId = '@EstacionAqui:places';
+const historicId = '@EstacionAqui:historic';
+const currentSpot = '@EstacionAqui:currentSpot';
+const userId = '@EstacionAqui:user';
+
+export async function getDBAllPlaces(){
+    // const netInfo = useNetInfo();
+
+    // await AsyncStorage.removeItem(placesId);
+    let response = await AsyncStorage.getItem(placesId);
+
+    let getDt = response ? JSON.parse(response) : [];
+
+    const getDtFormatted: ParkData[] = getDt ? getDt.map((item: ParkData) => { return item }) : getDt;
+
+    var respMap = new Map();
+      respMap = await getAPIAllPlaces(getDtFormatted);
+    /*if(netInfo.isConnected){
+      respMap = await getAPIAllPlaces(getDtFormatted);
+    }else{      
+      getDtFormatted.forEach(function(item : ParkData){
+          respMap.set(item.id, item);
+      }, {respMap})
+    }*/
+
+    var allParks : ParkData[];
+    allParks = [];
+
+    respMap.forEach(function(item){
+        allParks.push(item);
+    });
+
+    await AsyncStorage.setItem(placesId, JSON.stringify(allParks));
+
+    return allParks;
+}
+
+export async function getDBLastTravelAllPlaces(){
+    // const netInfo = useNetInfo();
+
+    let response = await AsyncStorage.getItem(historicId);
+
+    let getDt = response ? JSON.parse(response) : [];
+
+    var getDtFormatted: TravelData[] = getDt ? getDt.map((item: TravelData) => { return item }) : getDt;
+    // if(netInfo.isConnected){
+      
+      let responseUser = await AsyncStorage.getItem(userId);
+      let responseData : UserData;
+      if(responseUser){
+          responseData = JSON.parse(responseUser);
+      }else{
+        responseData = {
+          id: '',
+          name: '',
+          email: '',
+          password: '',
+          car: ''
+        };
+      }
+        
+      getDtFormatted = await lastTravels(responseData.id);
+    // }
+
+    await AsyncStorage.setItem(historicId, JSON.stringify(getDtFormatted));
+
+    return getDtFormatted;
+}
+
+export async function getDBEstabData(estabId : string){
+    // await AsyncStorage.removeItem(placesId);
+    let response = await AsyncStorage.getItem(placesId);
+
+    let getDt = response ? JSON.parse(response) : [];
+
+    const getDtFormatted: ParkData[] = getDt ? getDt.map((item: ParkData) => { return item }) : getDt;
     
-    async function getData(){
-      const getData = await AsyncStorage.getItem(dataKey);
-      return getData;
-    }
-    async function setData(allParks : ParkData[]){
-      await AsyncStorage.setItem(dataKey, JSON.stringify(allParks));
-    }
-    async function loadData(){
-      const getDt = getData();
-      getDt.then(function(val){
-        var data = JSON.parse(val as any) as ParkData[];
-        if(!data){
-          let allParks = getAPIAllPlaces(); 
-          setData(allParks).then(function(val){
-            console.log('Create Data');
-            return allParks;
-          }).catch(function(val){
-          });
-        }
-      }).catch(function(val){
-      });
-      return [];
-    }
+    let returnData : ParkData;
+    
+      returnData = {
+        id: '',
+        type: 'open',
+        title : '',
+        amount : '',
+        quantitySpots : '',
+        latitude : 0,
+        longitude : 0
+      };
+    getDtFormatted.forEach(function(item){
+      if(item.id == estabId){
+        returnData = item; 
+        return;
+      }
+    
+    });
+    return returnData;
 
-    return loadData();
+}
+
+export async function setUserData(id : string,name : string, car : string, email : string, password : string){
+  var respData = await sendUserData({id: id, name: name, car: car, email: email, password: password});
+
+  await AsyncStorage.setItem(userId, JSON.stringify(respData));
+  var returnData :  UserData;
+  returnData = respData;
+
+  return returnData;
+}
+
+export async function checkDataLogin(email : string, password : string){
+  var respData = await checkLogin(email, password);
+  var returnData :  UserData;
+  returnData = respData;
+
+  if(returnData.id)
+    await AsyncStorage.setItem(userId, JSON.stringify(respData));
+
+  return returnData;
+}
+
+export async function checkUserData(){
+  var returnData :  UserData;
+  returnData = {
+    id : "",
+    name : "",
+    car : "",
+    password : "",
+    email : ""
+  }
+  var returnDataJSON = await AsyncStorage.getItem(userId);
+  if(returnDataJSON != null)
+    returnData = JSON.parse(returnDataJSON);
+  else
+    return null;
+}
+export async function logouts(){
+  await AsyncStorage.removeItem(userId);
+}
+
+export async function confirmSpotDB(parkId : String, spotId : String, usId : String){
+  var respData = await confirmSpot(parkId, spotId, usId);
+  var returnData :  UserData;
+  returnData = respData;
+
+  if(returnData.id)
+    await AsyncStorage.setItem(userId, JSON.stringify(respData));
+
+  return returnData;
 }
